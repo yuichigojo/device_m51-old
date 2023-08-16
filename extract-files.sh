c4619ab955ff17f2ee573c3ab52741874abe1553
Okada32 <ruinedmind2001@gmail.com>
2023-08-16 18:43:24 +0200

    Init

---
+++ extract-files.sh
@@ -0,0 +1,65 @@
+set -e
+
+DEVICE=m51
+VENDOR=samsung
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+LINEAGE_ROOT="${MY_DIR}/../../.."
+
+HELPER="${LINEAGE_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libsec-ril.so)
+            sed -i 's/ril.dds.call.slotid/vendor.calls.slotid/g' "${2}"
+            ;;
+        vendor/lib64/libsec-ril-dsds.so)
+            sed -i 's/ril.dds.call.slotid/vendor.calls.slotid/g' "${2}"
+            ;;
+        vendor/lib64/hw/android.hardware.health@2.0-impl-2.1-samsung.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            ;;
+    esac
+}
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+SECTION=
+KANG=
+
+while [ "$1" != "" ]; do
+    case "$1" in
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -k | --kang)            KANG="--kang"
+                                ;;
+        -s | --section )        shift
+                                SECTION="$1"
+                                CLEAN_VENDOR=false
+                                ;;
+        * )                     SRC="$1"
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "${SRC}" ]; then
+    SRC=adb
+fi
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
+
+BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+"${MY_DIR}/setup-makefiles.sh"
---
+++ lineage_m51.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from m51 device
+$(call inherit-product, device/samsung/m51/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_m51
+PRODUCT_DEVICE := m51
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M515F
+PRODUCT_MANUFACTURER := samsung
+
+# Use the latest approved GMS identifiers
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=qssi \
+    PRODUCT_NAME=m51nsxx \
+    PRIVATE_BUILD_DESC="m51nsxx-user 12 SP1A.210812.016 M515FXXS4DVF2 release-keys"
+
+BUILD_FINGERPRINT := samsung/m51nsxx/qssi:12/SP1A.210812.016/M515FXXS4DVF2:user/release-keys
+
+# Flags
+TARGET_BOOT_ANIMATION_RES := 1080
